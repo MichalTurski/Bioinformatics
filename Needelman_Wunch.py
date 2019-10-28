ecca6e07e1f34b70cfc8852203222adc8e566e43
turskim <michal.antoni.turski@gmail.com>
2019-10-28 10:58:24 +0100

    Click input parsing.

--- Needelman_Wunch.py
+++ Needelman_Wunch.py
@@ -1,5 +1,17 @@
 import numpy as np
 import json
-import argparse
+import click
 
-parser = argparse.ArgumentParser()
+
+@click.command()
+@click.option('--seq1', "-a", help='First sequence.', type=click.File('r'))
+@click.option('--seq2', '-b', help='Second sequence.', type=click.File('r'))
+@click.option('--config_file', '-c', help='Config file.', type=click.File('r'))
+@click.option('--output', '-o', help='Output file', type=click.File('w'))
+def main(seq1, seq2, config_file, output):
+    config_json = config_file.read()
+    config = json.dumps(config_json)
+
+
+if __name__ == "__main__":
+    main()
